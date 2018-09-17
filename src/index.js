55814bf685ca8f85fe5bed5bd80153d52b0a0238
provedenceva <anyuta-provedenc@mail.ru>
2018-09-17 19:19:53 +0300

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ var  arr = str.split('');
+ var countfirst = 0;
+ var countsecond = 0;
+ for(var i = 0; i < arr.length; ++i){
+   if(arr[2] == ")" && arr[3] == "(")
+   return false;
+   else if (arr[2] == "|" && arr[3] == ")")
+   return false;
+   else if (arr[2] == "]" && arr[3] == ")")
+   return false;
+   else if (arr[2] == "]" && arr[3] == "[")
+   return false;
+   else if (arr[2] == "5" && arr[3] == "5")
+   return false;
+   else if (arr[2] == "8" && arr[3] == "8")
+   return false;
+ }
+ for(var i = 0; i < arr.length; ++i){
+    if(arr[i] == "(")
+        countfirst++;
+}
+for(var i = 0; i < arr.length; ++i){
+    if(arr[i] == ")")
+        countsecond++;
+}
+
+ if (countfirst == countsecond) {
+   return true
+ } else { return false
+   
+ }// your solution
 }
